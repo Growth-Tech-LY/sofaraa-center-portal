7315604847bf284d6b4fadeb60ea8becacfdcbb8
alhasangood <alhasan.jamal66@gmail.com>
2024-07-09 17:45:59 +0200

    pull

--- src/axios.ts
+++ src/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'https://sofaraa-clinte-back.growthtech.ly/api/',
+  baseURL: 'https:///sofaraa-portal-backend.growthtech.ly/api/',
   headers: {
     accept: '*/*',
     // 'Content-Type': 'multipart/form-data',
