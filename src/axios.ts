248bf748b20d0ce09d7e7b94898011b6a5aef0f2
ali-km99 <alidx09@gmail.com>
2024-07-16 14:21:45 +0200

    halls

--- src/axios.ts
+++ src/axios.ts
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
- // baseURL: 'https://sofaraa-portal-backend.growthtech.ly/api/',
- baseURL: 'http://localhost:5127/api/',
+  baseURL: 'https://sofaraa-portal-backend.growthtech.ly/api/',
+  //  baseURL: 'http://localhost:5127/api/',
 
   headers: {
     accept: '*/*',
